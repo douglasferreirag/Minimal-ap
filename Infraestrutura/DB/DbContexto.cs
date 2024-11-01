d7a0a3e398652aa06c8080a54f726c1114b5f4dc
unknown <douglas.ferreirag@gmail.com>
2024-10-31 22:54:14 -0300

    Aula6Completa

--- Infraestrutura/DB/DbContexto.cs
+++ Infraestrutura/DB/DbContexto.cs
@@ -2,11 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.Entidades;
 
 namespace minimal_api.Infraestrutura.DB
 {
-    public class DbContexto
+    public class DbContexto: DbContext
     {
-        
+
+            private readonly IConfiguration _configuraçãoAppSettings;
+
+            public DbContexto(IConfiguration configuraçãoAppSettings){
+
+                    _configuraçãoAppSettings = configuraçãoAppSettings;
+
+            }
+
+            public DbSet<Administrador> Administradores { get; set; } = default!;
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+
+                if(!optionsBuilder.IsConfigured) {
+
+                            var stringConexao = _configuraçãoAppSettings.GetConnectionString("mysql")?.ToString();
+
+                            if(!string.IsNullOrEmpty(stringConexao)) {
+
+                                optionsBuilder.UseMySql(    stringConexao, 
+                                                            ServerVersion.AutoDetect(stringConexao)
+                                                    );
+
+                            }
+
+                    
+                }
+
+              
+
+                
+            }
+
     }
 }
