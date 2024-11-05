786de01a335089e1209e8f5f02ac4271b9f7130c
unknown <douglas.ferreirag@gmail.com>
2024-11-04 22:26:24 -0300

    Veiculos

--- Infraestrutura/DB/DbContexto.cs
+++ Infraestrutura/DB/DbContexto.cs
@@ -23,45 +23,47 @@ namespace minimal_api.Infraestrutura.DB
 
             public DbSet<Administrador> Administradores { get; set; } = default!;
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Administrador>().HasData(
+            public DbSet<Veiculo> Veiculos { get; set; } = default!;
 
-                    new Administrador { 
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Administrador>().HasData(
 
-                        Id = 1,
-                        Email = "Administrador@test.com",
-                        Senha = "123456",
-                        Perfil = "Adm"
+                        new Administrador { 
+
+                            Id = 1,
+                            Email = "Administrador@test.com",
+                            Senha = "123456",
+                            Perfil = "Adm"
 
 
+                        }
+
+                );
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+                {
+
+                    if(!optionsBuilder.IsConfigured) {
+
+                                var stringConexao = _configuraçãoAppSettings.GetConnectionString("mysql")?.ToString();
+
+                                if(!string.IsNullOrEmpty(stringConexao)) {
+
+                                    optionsBuilder.UseMySql(    stringConexao, 
+                                                                ServerVersion.AutoDetect(stringConexao)
+                                                        );
+
+                                }
+
+                        
                     }
 
-            );
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            {
-
-                if(!optionsBuilder.IsConfigured) {
-
-                            var stringConexao = _configuraçãoAppSettings.GetConnectionString("mysql")?.ToString();
-
-                            if(!string.IsNullOrEmpty(stringConexao)) {
-
-                                optionsBuilder.UseMySql(    stringConexao, 
-                                                            ServerVersion.AutoDetect(stringConexao)
-                                                    );
-
-                            }
+                
 
                     
                 }
-
-              
-
-                
-            }
 
     }
 }
--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58144419cb6a1f5a6a9dc8aa3bb716a35ce7e52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c0d82ea35ba8edbb6d80c27e6a5f9e43349cd09")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
