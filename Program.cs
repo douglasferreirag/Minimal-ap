36bc8b5564c3b2395bd07f2646f20ef7b4412213
unknown <douglas.ferreirag@gmail.com>
2024-11-10 23:11:26 -0300

    Put

--- Program.cs
+++ Program.cs
@@ -101,6 +101,27 @@ app.MapGet("/veiculos/{id}",  ([FromRoute] int id, IVeiculoServico veiculoServic
 
 }).WithTags("Veiculos");
 
+app.MapPut("/veiculos/{id}",  ([FromRoute] int id, VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico ) => {
+
+   var veiculo = veiculoServico.BuscaPorId(id);
+
+   if(veiculo == null)
+
+    return Results.NotFound();
+
+   veiculo.Nome = veiculoDTO.Nome;
+
+   veiculo.Marca = veiculoDTO.Marca;
+
+   veiculo.Ano = veiculoDTO.Ano;
+
+   veiculoServico.Atualizar(veiculo); 
+
+   return Results.Ok(veiculo);
+
+
+}).WithTags("Veiculos");
+
 
 #endregion
 
--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b535db62f6dc83e05a1f127f85f22c3f4c8707ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3499d07c3e8506e49347412ba2ca5d0845b437d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
