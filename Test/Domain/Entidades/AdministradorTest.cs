e7f377cfbd15de6cdab887bd3eeca30794262362
unknown <douglas.ferreirag@gmail.com>
2024-12-12 22:40:53 -0300

    incluindo testclass

--- Test/Domain/Entidades/AdministradorTest.cs
+++ Test/Domain/Entidades/AdministradorTest.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using minimal_api.Dominio.Entidades;
 
+
 namespace Test.Domain.Entidades
 {
+    [TestClass]
     public class AdministradorTest
     {
 
-         [TestMethod]
+        [TestMethod]
         public void TestarGetSetPropriedades()
         {
         
